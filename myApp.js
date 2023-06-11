5a5e78eb0ca06e69915365a9990d1ebe1fe78196
Dominik Popławski <poplawd@gmail.com>
2023-06-11 19:45:23 +0200

    add express.static middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 
 const filePath = __dirname + "/views/index.html"
+let publicFolderPath = __dirname + "/public"
+
+app.use("/public", express.static(publicFolderPath))
 
 app.get("/", (req, res) => {
   res.sendFile(filePath)
