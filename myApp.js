47ed86e13a95f562d53e387c4e01188825657a3b
Dominik Popławski <poplawd@gmail.com>
2023-06-11 18:46:39 +0200

    change get endpoint to server html file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+const filePath = __dirname + "/views/index.html"
+
+app.get("/", (req, res) => {
+  res.sendFile(filePath)
+})
 
 app.get("/", (req, res) => {
     res.send("Hello Express")
